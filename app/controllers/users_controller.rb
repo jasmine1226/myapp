9a0fbc7c232418ab95c7bab54ed43bb39bf4eba9
jasmine1226 <jasmine1226@gmail.com>
2014-03-15 21:15:56 +0800

    make user_params a private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,8 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  private
+  
   def user_params
     params.require(:user).permit(:accout, :password, :nickname, :realname, :email, :birthday)
   end
